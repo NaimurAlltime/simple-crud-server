c1add05a87955857438adf5bd23272e9173224c5
Naimur Rahman <naimur2231@gmail.com>
2023-05-11 17:02:04 +0600

    create post, update, delete api with crud operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -36,12 +36,47 @@ async function run() {
         res.send(result);
     })
 
+    // create get api with specific id 
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    })
+
     // post api create 
     app.post('/users', async(req, res) => {
         const user = req.body;
         console.log('new user', user);
         const result = await userCollection.insertOne(user);
         res.send(result);
+    })
+
+    // put api create (updated)
+    app.put('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(id, user);
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser, options);
+      res.send(result);
+    })
+
+
+    // delete api 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log("please delete from database", id);
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     })
 
 
